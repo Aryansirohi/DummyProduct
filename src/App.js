e2defa4b90604707ab6e2f31329f16f840b784da
Aryan Sirohi <99206576+Aryansirohi@users.noreply.github.com>
2024-09-28 19:58:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import CategoryList from './components/CategoryList';
+import ProductList from './components/ProductList';
+import SearchBar from './components/SearchBar';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div>
+        <SearchBar />
+        <CategoryList />
+        <ProductList />
+      </div>
+    </Provider>
   );
-}
+};
 
 export default App;
